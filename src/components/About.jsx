eb532a17ea7ff4c8bdd6018b2a7a6f3d51e2fa6a
MatiasMontenegrom <matiasmontenegrom@gmail.com>
2022-06-21 23:00:16 -0300

    commit

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -18,7 +18,7 @@ export const About = () => {
                 <div className='max-w-[1000px] w-full grid 
                         sm:grid-cols-2 gap-8 px-4'>
                     <div className='sm:text-right text-4xl font-bold '>
-                        <p>Soy Matiasss, encantado de conocerte.</p>
+                        <p>Soy Matias, encantado de conocerte.</p>
                     </div>
                     <div>
                         <p text-xl>Desde que tengo memoria me encanta la tecnología, cuando conocí la programación fue algo mágico y sin dudas era algo de lo que quería aprender. Cada día intento adquirir conocimiento para lograr cada vez mayores hazañas dentro de este gran mundo.!</p>
